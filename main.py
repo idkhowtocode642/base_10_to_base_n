605ac450a9abde4a9884c42c006d19b240a6537b
idkhowtocode642 <59237849+idkhowtocode642@users.noreply.github.com>
2022-07-26 21:55:06 +1000

    UPdate 1 (one line removed)

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 def base_10_to_base_n(value: int, base: int):
     def operation(value: int, base: int, result=[]):
-        if value == 0:
-            return result
-        else:
+        if value != 0:
             print(base, value - (value % base), value % base)
             result.insert(0, value % base)
             return operation(int((value - (value % base)) / base), base)
+        return result
     print("Result", operation(value, base))
